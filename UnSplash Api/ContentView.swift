5753d896110989bfc1231c1d937e01d568053569
ilies ouldmenouer <ilyasdzair1@gmail.com>
2021-09-08 16:51:12 +0100

    SearchPhoto by keyWord

--- UnSplash Api/ContentView.swift
+++ UnSplash Api/ContentView.swift
@@ -26,6 +26,8 @@ struct Home : View {
     @State var expand = false
     @State var search = ""
     @ObservedObject var RandomImages = getData()
+    @State var page = 1
+    @State var isSearching = false
     
     var body: some View {
         
@@ -70,6 +72,13 @@ struct Home : View {
                             self.expand = false
                         }
                         
+                        if self.isSearching {
+                            self.search = ""
+                            self.isSearching = false
+                            self.RandomImages.images.removeAll()
+                            self.RandomImages.updateData()
+                        }
+                        
                     }){
                         Image(systemName: "xmark")
                             .font(.system(size: 15,weight: .bold))
@@ -81,7 +90,11 @@ struct Home : View {
                 
                 if self.search != "" {
                     Button(action: {
-                        
+//                        delete old list
+                        self.RandomImages.images.removeAll()
+                        self.isSearching = true
+//                        Search for image by name
+                        self.SearchData()
                     }){
                         Text("Find")
                             .fontWeight(.bold)
@@ -154,9 +167,49 @@ struct Home : View {
                                 }
                             }
                             
+                            
                         }
                         
+                        if !self.RandomImages.images.isEmpty{
+                            
+                            if self.isSearching && self.search != "" {
+                                
+                                HStack{
+                                    Text("Page\(self.page)")
+                                    Spacer()
+                                    Button(action: {
+                                        self.RandomImages.images.removeAll()
+                                        self.page += 1
+                                        self.SearchData()
+                                    })
+                                    {
+                                        Text("Next")
+                                    }
+                                }
+                                .padding(.horizontal,25)
+                                
+                            }else{
+                                HStack{
+                                    Spacer()
+                                    Button(action: {
+                                        self.RandomImages.images.removeAll()
+                                        self.RandomImages.updateData()
+                                    })
+                                    {
+                                        Text("Next")
+                                    }
+                                }
+                                .padding(.horizontal,25)
+                            }
+                        }
+                        
+                        
+                        
+
+                        
+                        
                     }
+                    
                     .padding(.top)
                     
                 }
@@ -166,9 +219,25 @@ struct Home : View {
         .background(Color.black.opacity(0.07).edgesIgnoringSafeArea(.all ))
         .edgesIgnoringSafeArea(.top)
     }
+    
+    
+    func SearchData()  {
+        let key = "gKc-uC-4eHiLAK3qhJmfhVb-kz2RcaOU_f7DJDLvGWo"
+        let query = self.search.replacingOccurrences(of: " ", with: "%20")
+        let url = "https://api.unsplash.com/search/photos/?page=\(self.page)&query=\(query)&client_id=\(key)"
+        
+        print(url)
+        
+        self.RandomImages.SearchData(url : url)
+    }
 }
 
 
+
+
+
+
+//Fetch data
 
 class getData : ObservableObject {
     
@@ -179,6 +248,8 @@ class getData : ObservableObject {
         //        initial data
         updateData()
     }
+    
+    
     
     func updateData()  {
         
@@ -223,6 +294,54 @@ class getData : ObservableObject {
         .resume()
     }
     
+    
+    
+    func SearchData(url : String)  {
+        
+       
+        let session = URLSession(configuration: .default)
+        
+        print("search link : \(url)")
+        
+        session.dataTask(with: URL(string: url)!) { (data, _ ,err) in
+            
+            if err != nil {
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            //            JSON Decoding
+            
+            do {
+                
+                let json = try JSONDecoder().decode(SearchPhoto.self, from: data!)
+                
+                for i in stride(from: 0, to: json.results.count, by: 2){
+                    
+                    var ArrayData : [Photo] = []
+                    
+                    for j in i..<i+2 {
+                        
+                        if j < json.results .count {
+                            ArrayData.append(json.results[j])
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        self.images.append(ArrayData)
+                    }
+                }
+                
+            }catch{
+                print("Catch err ....")
+                print(error.localizedDescription)
+            }
+        }
+        .resume()
+    }
+    
+
+    
+    
 }
 
 struct Photo : Identifiable,Decodable,Hashable {
@@ -230,6 +349,14 @@ struct Photo : Identifiable,Decodable,Hashable {
     var id : String
     var urls : [String : String]
 }
+
+
+//Search result model
+struct SearchPhoto : Decodable {
+    
+    var results : [Photo]
+}
+
 
 struct Indicator : UIViewRepresentable {
     
